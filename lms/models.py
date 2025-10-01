292391669ad3830d9871644707674548c54d44b2
Denis Safonov <denissafonov_85@mail.ru>
2025-10-01 21:12:58 +0500

    В приложении lms реализованы модели Course и Lesson

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,35 @@
+from symtable import Class
+
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    """ Описание полей модель - курс """
+
+    name = models.CharField(max_length=100, verbose_name="Название", help_text="Введите название курса")
+    image = models.ImageField(upload_to="media/", verbose_name="Превью", help_text="Загрузите картинку (превью)")
+    description = models.TextField(verbose_name="Описание курса", help_text="Введите описание курса")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    """ Описание полей модель - урок """
+
+    name = models.CharField(max_length=100, verbose_name="Название", help_text="Введите название урока")
+    description = models.TextField(verbose_name="Описание урока", help_text="Введите описание урока")
+    image = models.ImageField(upload_to="media/", verbose_name="Превью", help_text="Загрузите картинку (превью)")
+    link_to_video = models.CharField(max_length=150, verbose_name="Ссылка на видео", help_text="Введите ссылку на видео")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс", help_text='Выберите курс', related_name='lesson')
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return self.name
+
---
+++ lms/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .apps import LmsConfig
+
+app_name = LmsConfig.name
+
+urlpatterns = [
+
+]
