647bc6cff870093c78acf966ac3a4dae659162d4
Denis Safonov <denissafonov_85@mail.ru>
2025-10-01 20:48:45 +0500

    Создана миграция модели User

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(AbstractUser):
     """ Описание полей модель пользователь """
 
-    email = models.EmailField(verbose_name="Почта", help_text="Введите почту")
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Введите почту")
     phone = models.CharField(max_length=15, verbose_name="Номер телефона", help_text="Введите номер телефона", blank=True, null=True)
     city = models.CharField(max_length=50, verbose_name="Город", help_text="Введите город", blank=True, null=True)
     avatar = models.ImageField(upload_to="users/avatar/", blank=True, null=True)
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps import UsersConfig
+from .apps import UsersConfig
 
 app_name = UsersConfig.name
 
