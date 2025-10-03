aab90d1f371fdfac9c24acc1bfa8dacf67e2d482
Denis Safonov <denissafonov_85@mail.ru>
2025-10-03 20:35:28 +0500

    В сериализаторе CourseSerializer ревлизован вывод уроков

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,18 +3,21 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_number = serializers.SerializerMethodField()
+    lesson = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Course
-        fields = ("id", "name", "image", "description", "lessons_number")
+        fields = ("id", "name", "image", "description", "lesson", "lessons_number")
 
     def get_lessons_number(self, instance):
         """Функция подсчитывает количество уроков в курсе"""
         return Lesson.objects.filter(course=instance).count()
 
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
+
