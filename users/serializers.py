ae0cd45ef243c3db0322e284df5db8c8928c5fa6
Denis Safonov <denissafonov_85@mail.ru>
2025-10-06 11:12:05 +0500

    Для профиля пользователя реализован вывод истории платежей

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 from .models import User, Payments
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
 class PaymentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payments
         fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    payment = PaymentsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "phone", "city", "avatar", "payment")
