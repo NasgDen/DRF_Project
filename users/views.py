a66b87539f96ae7117e46763f8686b6ecd249054
Denis Safonov <denissafonov_85@mail.ru>
2025-10-07 20:20:35 +0500

    Добавлен permission IsModerator в permissions.py

--- users/views.py
+++ users/views.py
@@ -14,6 +14,11 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
     def get_permissions(self):
         if self.action == "create":
             self.permission_classes = (AllowAny,)
