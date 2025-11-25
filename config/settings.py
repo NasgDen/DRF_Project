0da31651c385881fb202246c0d9d801f7fad7cb2
Denis Safonov <denissafonov_85@mail.ru>
2025-11-25 20:57:21 +0500

    settings.py - изменено DEBUG, ci.yml - удалена проверочная информация

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("CORS_ALLOW_ALL_ORIGINS", "DEBUG") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
